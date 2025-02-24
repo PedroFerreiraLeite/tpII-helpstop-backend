bda1c0ac451074ab8ddd9895329df860bb3f1d0c
Pedro Ferreira Leite <pedro1000.leite@gmail.com>
2025-02-24 12:34:16 -0300

    Criação básica dos dominios de entidade

---
+++ HelpApp/HelpApp.Domain/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using HelpApp.Domain.Validation;
+
+
+namespace HelpApp.Domain.Entities
+{
+    public class Category
+    {
+        #region atibutos
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region constructors
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+        public ICollection<Product> Products { get; set; }
+        #endregion
+        #region Validation
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minum 3 characters.");
+
+            Name = name;
+        }
+        #endregion
+    }
+}
